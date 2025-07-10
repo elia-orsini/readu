afa7acff787872195004ae5145c238e1f04c05d2
Your name here <elia.orsini@hotmail.com>
2025-07-10 18:55:15 +0800

    fix: highlight button

--- src/components/reading/HighlightButton.tsx
+++ src/components/reading/HighlightButton.tsx
@@ -41,9 +41,9 @@ export default function HighlightButton({
       {showPopup && (
         <div
           className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50"
-          onClick={() => setShowPopup(false)} // Close when clicking outside
+          onClick={() => setShowPopup(false)}
         >
-          <div className="m-4 flex max-w-md flex-col gap-4 rounded-lg border border-foreground bg-background p-6 shadow-xl">
+          <div className="m-4 flex max-w-md flex-col gap-4 rounded-lg border border-foreground bg-background px-20 py-10 shadow-xl">
             <p className="text-center text-lg">Who are you?</p>
             <div className="flex flex-col gap-2">
               {readingGroupMembers.map((member, i) => (
@@ -65,17 +65,19 @@ export default function HighlightButton({
         </div>
       )}
 
-      <button
-        onClick={handleHighlightClick}
-        onTouchEnd={handleHighlightClick}
-        className={`fixed bottom-10 z-50 rounded border border-foreground !bg-black px-4 py-2 text-background sm:left-1/2 sm:-translate-x-1/2 sm:transform sm:text-background sm:bg-[var(--${
-          currentUser
-            ? highlightsColours[readingGroupMembers.indexOf(currentUser)].cssVar
-            : highlightsColours[0].cssVar
-        })]`}
-      >
-        Highlight
-      </button>
+      {!showPopup && (
+        <button
+          onClick={handleHighlightClick}
+          onTouchEnd={handleHighlightClick}
+          className={`fixed bottom-10 z-50 rounded border border-foreground !bg-black px-4 py-2 text-background sm:left-1/2 sm:-translate-x-1/2 sm:transform sm:text-background sm:bg-[var(--${
+            currentUser
+              ? highlightsColours[readingGroupMembers.indexOf(currentUser)].cssVar
+              : highlightsColours[0].cssVar
+          })]`}
+        >
+          Highlight
+        </button>
+      )}
     </div>
   );
 }
