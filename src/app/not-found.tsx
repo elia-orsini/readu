7f52631128fe06ea607be28814550d71c992b298
Your name here <elia.orsini@hotmail.com>
2025-07-10 15:56:55 +0800

    fix: offline 404

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,12 +1,12 @@
 import AlwaysWhiteHeader from "@/components/header/AlwaysWhiteHeader";
 
-export default async function Offline() {
+export default async function NotFound() {
   return (
     <div className="flex h-dvh w-screen flex-col">
       <AlwaysWhiteHeader />
 
       <div className="flex h-full w-full">
-        <p className="m-auto">it seems like you are offline</p>
+        <p className="m-auto">this page does not exist</p>
       </div>
     </div>
   );
